c144d156a18680a4d40df8b2a411883bf014d717
Hetenyi <hetenyibalint06@gmail.com>
2023-05-31 22:00:46 +0200

    initial commit

---
+++ májusi progfeladat/Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace májusi_progfeladat
+{
+    public class user
+    {
+
+
+        private static user instance;
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        private user() { }
+
+        public static user Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new user();
+                }
+                return instance;
+            }
+        }
+        /*public string name { get; set; }
+        public string pass { get; set; }
+
+
+        public static user UserQuery(string nev, string jelszo)
+        {
+
+            user felhasz = null;
+            felhasz.name = nev;
+            felhasz.pass = jelszo;
+        }
+
+        //public string Nev { get => name; set => name = value; }
+        //public string Jelszo { get => pass; set => pass = value; }*/
+    }
+}
